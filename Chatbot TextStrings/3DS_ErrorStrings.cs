ad4137eba85cb0ffe50922b6c8a553278a6168ec
TerrysaurusRex18 <88121674+TerrysaurusRex18@users.noreply.github.com>
2021-07-30 12:01:57 -0400

    Change blanks to xxx's in format, change try to request

--- Chatbot TextStrings/3DS_ErrorStrings.cs
+++ Chatbot TextStrings/3DS_ErrorStrings.cs
@@ -4,8 +4,8 @@
     {
         public static string InvalidDepoit = "Invalid deposit Pokémon!";
         public static string NoEntriesFound = "It appears there are no entries of your deposit Pokémon, request again with a different Pokémon! (ERROR: NO ENTRIES FOUND!)";
-        public static string NoRequestOnListFound = "Can't find any Pokémon from your request in the list, please try again!";
-        public static string DepositNotFound = "Your deposit Pokémon was not found, please try again! Check '!help gts' for troubleshooting steps! Remember, mythical Pokémon can't be requested from the Gen 7 (SUMO/USUM) GTS trades!";
+        public static string NoRequestOnListFound = "Can't find any Pokémon from your request in the list, please request again!";
+        public static string DepositNotFound = "Your deposit Pokémon was not found, please request again! Check '!help gts' for troubleshooting steps! Remember, mythical Pokémon can't be requested from the Gen 7 (SUMO/USUM) GTS trades!";
         public static string PossibleAlreadyTraded = "Pokémon was already traded?";
 
         /* Prolly Deleted when new Change result in a failed trade */
@@ -17,7 +17,7 @@
         public static string TradeCanceledGenderNotAny = ", Trade cancelled, you selected Gender: {0} in the GTS, but the Gender should be set to Any!";
 
         public static string AlreadyTradedOrInvalidException = "Pokémon has already been traded by another person or you probably made invalid customizations!";
-        public static string FatalErrorException = "Fatal error occurred, please try again!";
+        public static string FatalErrorException = "Fatal error occurred, please request again!";
 
         public static string InvalidChangesAbortTrade = "Your provided customizations were invalid. Trade cancelled!";
     }
--- Chatbot TextStrings/SWSH_ErrorStrings.cs
+++ Chatbot TextStrings/SWSH_ErrorStrings.cs
@@ -5,14 +5,14 @@
 
         public static class ErrorStrings
         {
-            public static string NotOnOverworldAfterStart = "We had trouble starting the search, please try again!";
+            public static string NotOnOverworldAfterStart = "We had trouble starting the search, please request again!";
             public static string NoTradePartnerFound = "No trade partner found!";
             public static string NoTradePartnerFound2 = "No trade partner found, please request again!";
             public static string WaitedTooLong = "You waited too long, trade aborted!";
             public static string NoBoxVisible = "Box was never shown, please request again!";
             public static string FailedTradePIDMatch = "Something is wrong with your Pokémon or you declined the trade!";
 
-            public static string FatalErrorException = "Fatal error occurred, please try again!";
+            public static string FatalErrorException = "Fatal error occurred, please request again!";
 
             public static string InvalidChangesAbortTrade = "Your provided customizations were invalid. Trade cancelled!";
         }
