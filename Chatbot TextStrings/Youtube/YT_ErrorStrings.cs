942753fc80cdef7ae26a04c43766f2d071e1fd3a
Chris <chris.eitelmann@gmail.com>
2021-11-17 03:06:39 +0100

    Update YT_ErrorStrings.cs

--- Chatbot TextStrings/Youtube/YT_ErrorStrings.cs
+++ Chatbot TextStrings/Youtube/YT_ErrorStrings.cs
@@ -8,5 +8,7 @@
         public static string YT_Evo_Clone_Error = "The trade evolution request from {0} is not possible. Trade cancelled!";
         public static string YT_Cleaning_Clone_Error = "The name clearing request from {0} is not possible. Trade cancelled!";
         public static string YT_Not_In_Galar = "@{0} This Pokémon doesn't exist in Galar!";
+        public static string YT_FC_Banned = "@{0} your Friendcode is banned, you're not allowed to trade here anymore.";
+        public static string YT_NNID_Banned = "@{0} your Nintendo Network ID is banned, you're not allowed to trade here anymore.";
     }
 }
