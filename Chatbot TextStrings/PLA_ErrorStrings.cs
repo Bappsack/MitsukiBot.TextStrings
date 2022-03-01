45e32b9e54749cafd06de26435ac4e7f3a9ed87e
Bappsack <chris.eitelmann@gmail.com>
2022-03-02 00:03:18 +0100

    Revert "Update PLA_ErrorStrings.cs"
    This reverts commit 66fc6a8614ca5779e22d70124b18fd64ee2760d4.

--- Chatbot TextStrings/PLA_ErrorStrings.cs
+++ Chatbot TextStrings/PLA_ErrorStrings.cs
@@ -21,8 +21,8 @@
 
         public static class ResponseStrings
         {
-            public static string UserTurnStarted = "/me VoteYea @{0} Its your turn now, please start searching, you have 60 seconds to match with me! VoteYea";
-            public static string UserFound = "@{0} I found you, Trainer Name: {1}, waiting until you select a Pokemon! If that isn't you, please change your Link Code to a harder one!";
+            public static string UserTurnStarted = "/me VoteYea @{0} It's your turn now, please start searching, you have 60 seconds to match with me! VoteYea";
+            public static string UserFound = "@{0} I found you, Trainer Name: {1}, waiting until you select a Pokémon! If that isn't you, please change your Link Code to a harder one!";
         }
     }
 }
