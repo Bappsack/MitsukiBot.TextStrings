bb65dfd19dbf5f8980ee445b516607159a306325
Bappsack <chris.eitelmann@gmail.com>
2022-04-21 23:34:25 +0200

    rewrite form list + logic

--- Chatbot TextStrings/Youtube/YT_ErrorStrings.cs
+++ Chatbot TextStrings/Youtube/YT_ErrorStrings.cs
@@ -9,6 +9,7 @@
         public static readonly string YT_Cleaning_Clone_Error = "@{0} Your name clearing request is not possible. Trade cancelled!";
         public static readonly string YT_Not_In_Galar = "@{0} Your requested Pokémon doesn't exist in Galar!";
         public static readonly string YT_Not_In_Sinnoh = "@{0} Your requested Pokémon doesn't exist in Sinnoh!";
+        public static readonly string YT_Not_In_Hisui = "@{0} Your requested Pokémon doesn't exist in Hisui!";
         public static readonly string YT_FC_Banned = "@{0} Your friend code is banned, you're not allowed to trade here anymore.";
         public static readonly string YT_NNID_Banned = "@{0} Your Nintendo Network ID is banned, you're not allowed to trade here anymore.";
     }
