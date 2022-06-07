885db71bc5bec652ba1342b055183837ef40e1b0
Bappsack <chris.eitelmann@gmail.com>
2022-06-07 06:50:42 +0200

    Update 3DS_ErrorStrings.cs

--- Chatbot TextStrings/3DS_ErrorStrings.cs
+++ Chatbot TextStrings/3DS_ErrorStrings.cs
@@ -8,15 +8,16 @@
         public readonly static string DepositNotFound = "Your deposit Pokémon was not found, please request again! Check '!help gts' for troubleshooting steps or ask for help in the #support channel in our Discord server!";
         public readonly static string PossibleAlreadyTraded = "Pokémon was already traded?";
 
-        /* Prolly Deleted when new Change result in a failed trade */
         public readonly static string InvalidChangesDefault = "Invalid customizations, trading default Pokémon if no level was set, otherwise see this as a failed trade!";
         public readonly static string LegalityIssuesAfterbuild = "Something is wrong with your request!";
 
         public readonly static string Mythicals6PastGen = "Your requested mythical Pokémon is not from a Gen 6 event, so it cannot be traded via the GTS! Trade cancelled!";
 
-        public readonly static string TradeCanceledGenderLevelNotAny = ", Trade cancelled, you selected Gender: {0} and Level: {1} in the GTS, but the Gender and Level should be set to Any!";
-        public readonly static string TradeCanceledLevelNotAny = ", Trade cancelled, you selected Level: {0} in the GTS, but the Level should be set to Any!";
-        public readonly static string TradeCanceledGenderNotAny = ", Trade cancelled, you selected Gender: {0} in the GTS, but the Gender should be set to Any!";
+        public readonly static string TradeCanceledGenderLevelNotAny = "Trade cancelled, your wanted Details are invalid! Level & Gender should be set to Any!";
+        public readonly static string TradeCanceledLevelNotAny = "Trade cancelled, your wanted Details are invalid! Level should be set to Any!";
+        public readonly static string TradeCanceledGenderNotAny = "Trade cancelled, your wanted Details are invalid! Gender should be set to Any!";
+
+        public readonly static string TradeCancelBannedFriendcode = "Trade cancelled, your Friendcode is banned!";
 
         public readonly static string AlreadyTradedOrInvalidException = "Pokémon has already been traded by another person or you probably made invalid customizations!";
         public readonly static string FatalErrorException = "Fatal error occurred, please request again!";
