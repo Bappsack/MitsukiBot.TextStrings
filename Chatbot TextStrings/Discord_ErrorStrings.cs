c46ddf1da932cf5d558dcdcde2e2902bf7d30831
Bappsack <chris.eitelmann@gmail.com>
2022-07-12 03:45:22 +0200

    Create Discord_ErrorStrings.cs

---
+++ Chatbot TextStrings/Discord_ErrorStrings.cs
@@ -0,0 +1,17 @@
+﻿namespace Chatbot_TextStrings
+{
+    public static class Discord_TextStrings
+    {
+        public static class ErrorStrings
+        {
+            public readonly static string IllegalFileProvided = "{0} your prodived File is illegal, trade aborted!";
+
+            public readonly static string InvalidFileTypeGeneral = "{0} your provided file is not supported. Trade cancelled!";
+            public readonly static string InvalidFileTypePK6 = "{0} your provided file is not supported in XY/ORAS, please use a .PK6 file. Trade cancelled!";
+            public readonly static string InvalidFileTypePK7 = "{0} your provided file is not supported in SM/USUM, please use a .PK7 file. Trade cancelled!";
+            public readonly static string InvalidFileTypePK8 = "{0} your provided file is not supported in SWSH, please use a .PK8 file. Trade cancelled!";
+            public readonly static string InvalidFileTypePB8 = "{0} your provided file is not supported in BDSP, please use a .PB8 file. Trade cancelled!";
+            public readonly static string InvalidFileTypePA8 = "{0} your provided file is not supported in PLA, please use a .PA8 file. Trade cancelled!";
+        }
+    }
+}
