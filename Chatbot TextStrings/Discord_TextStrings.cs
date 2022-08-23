ad13b9654249067caa8883de76517549a1ffb339
Bappsack <chris.eitelmann@gmail.com>
2022-08-23 04:52:01 +0200

    Update Discord_TextStrings.cs

--- Chatbot TextStrings/Discord_TextStrings.cs
+++ Chatbot TextStrings/Discord_TextStrings.cs
@@ -9,6 +9,7 @@
             public readonly static string InvalidFileTypeGeneral = "{0} your provided file is not supported. Trade cancelled!";
             public readonly static string InvalidFileTypePK6 = "{0} your provided file is not supported in XY/ORAS, please use a .PK6 file. Trade cancelled!";
             public readonly static string InvalidFileTypePK7 = "{0} your provided file is not supported in SM/USUM, please use a .PK7 file. Trade cancelled!";
+            public readonly static string InvalidFileTypePB7 = "{0} your provided file is not supported in LGPE, please use a .PB7 file. Trade cancelled!";
             public readonly static string InvalidFileTypePK8 = "{0} your provided file is not supported in SWSH, please use a .PK8 file. Trade cancelled!";
             public readonly static string InvalidFileTypePB8 = "{0} your provided file is not supported in BDSP, please use a .PB8 file. Trade cancelled!";
             public readonly static string InvalidFileTypePA8 = "{0} your provided file is not supported in PLA, please use a .PA8 file. Trade cancelled!";
