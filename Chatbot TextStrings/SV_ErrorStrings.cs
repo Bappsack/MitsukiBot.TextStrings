adf8b3dccfc83a730efd9f8646b6f7b0e2bd921d
Bappsack <chris.eitelmann@gmail.com>
2022-11-29 03:12:12 +0100

    Update SV_ErrorStrings.cs

--- Chatbot TextStrings/SV_ErrorStrings.cs
+++ Chatbot TextStrings/SV_ErrorStrings.cs
@@ -30,7 +30,7 @@
 
         public static class ResponseStrings_Discord
         {
-            public readonly static string UserTurnStarted = "<@{0}> It's your turn now, please start searching, you have 60 seconds to match with me!";
+            public readonly static string UserTurnStarted = "<@{0}> It's your turn now, please start searching, you have 40~ seconds to match with me!";
             public readonly static string UserFound = "<@{0}> I found you, Trainer Name: {1}, waiting until you select a Pokémon! If that isn't you, please change your Link Code to a harder one!";
         }
     }
