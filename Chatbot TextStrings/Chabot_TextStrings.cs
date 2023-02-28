8217e218cd4cc81835572965f034fb8dbe169adc
Chris <chris.eitelmann@gmail.com>
2023-02-28 14:50:19 +0100

    Update Chabot_TextStrings.cs

--- Chatbot TextStrings/Chabot_TextStrings.cs
+++ Chatbot TextStrings/Chabot_TextStrings.cs
@@ -112,6 +112,11 @@ namespace TextStrings
             public readonly static string EvolvingDepositMon = "You can't use a Pokémon that evolves via trade as the deposit Pokémon!";
             public readonly static string MegaStonesSubOnly = "Only subscribers can request Mega Stones!";
             public readonly static string IsEggSubOnly = "Only subscribers can request Eggs!";
+            public readonly static string TIDSubOnly = "Only subscribers can use TID!";
+            public readonly static string SIDSubOnly = "Only subscribers can use SID!";
+            public readonly static string OTGenderSubOnly = "Only subscribers can use OTGender!";
+            public readonly static string TakeOwnSubOnly = "Only subscribers can use TakeOwn!";
+
             public readonly static string MegaStones_NonExistPastGen7 = "Mega Stones do not exist in this game!";
             public readonly static string TooManyErrorsToPrint = "Too many invalid commands! Make sure you didn't forget the commas!";
             public readonly static string Exception = "Wrong/bad command usage! Make sure you didn't forget the commas!";
