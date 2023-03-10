ccc6489f614c697e3385c092a24c8af3e7440b6e
Chris <chris.eitelmann@gmail.com>
2023-03-10 15:06:57 +0100

    tweak TerryAI

--- Chatbot TextStrings/Discord_TextStrings.cs
+++ Chatbot TextStrings/Discord_TextStrings.cs
@@ -34,6 +34,11 @@ namespace Chatbot_TextStrings
         {
             internal static readonly RegexOptions options = RegexOptions.IgnoreCase | RegexOptions.Multiline;
 
+            internal static ulong GeneralChat = 312692099264937984;
+            internal static ulong TradeRoom = 313696378398310401;
+            internal static ulong SubRoom = 582336509890265088;
+            internal static ulong Support = 629335412141195264;
+
             internal static class Needles
             {
                 internal static readonly string BotQuestionList_NonSupport = @"^(?=.*\b(what[\W?s]*|when[\W?s]*|where[\W?s]*|why[\W?s]*|how[\W?s]*|will|won\W?t|did[n\W?t]*|do[esn\W?t]*|can[n?o?\W?t]*|problems?|issues?|is\sit\spossible)\b)(?=.*\b(bots?|commands?|forms?|roles?|cool\s?downs?|[link]*code|sub[scribe]*|deposit[seding]*|request[seding]*|work[seding]*|invalid|pcoins|online|offline|banned)\b)(?!.*\b(raids?|evolv[seding]*|[poke]*dex|sand?wh?it?ch)\b).*$";
@@ -43,10 +48,10 @@ namespace Chatbot_TextStrings
             {
                 internal static readonly string BotQuestionNonSupport = "Your message was flagged as a possible support question. **If you need help with the bots, streams, or server, please ask in <#629335412141195264>!** If you did not ask a support question, please disregard this message.";
             }
-
-            public static string? GetResponderString(string Haystack, bool IsInSupportChannel, bool OnlyGeneral) => Haystack switch
+            
+            public static string? GetResponderString(string Haystack, ulong ChannelID) => Haystack switch
             {
-                var h when (!IsInSupportChannel && OnlyGeneral && Regex.IsMatch(h, Needles.BotQuestionList_NonSupport, options)) => RespondStrings.BotQuestionNonSupport,
+                var h when ( ChannelID == GeneralChat && ChannelID != Support && Regex.IsMatch(h, Needles.BotQuestionList_NonSupport, options)) => RespondStrings.BotQuestionNonSupport,
                 _ => null,
             };
         }
